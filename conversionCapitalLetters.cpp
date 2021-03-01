68ea7cd380e3903156f547d19c59d33fe97e4708
miki <xograpesxo@gmail.com>
2021-03-01 21:36:39 +0900

    add conversionCapitalLetters.cpp

---
+++ conversionCapitalLetters.cpp
@@ -0,0 +1,25 @@
+﻿/*
+----------------------------------------------------------
+参考サイト
+----------------------------------------------------------
+http://kitako.tokyo/lib/CExercise.aspx?id=8
+練習問題8-2
+*/
+
+
+
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+	char str[] = "hello world"; 
+
+	for (int i = 0; i < strlen(str); i++) {
+		if (i == 0 || i == 6) {
+			*(str + i) = *(str + i) - ('a' - 'A');
+		}
+	}
+	printf("%s\n", str);
+
+}
